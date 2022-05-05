23c7b21e01e701d2d137d04d323511e204a6bc6a
ABOUZID <bouzidanas@yahoo.fr>
2022-05-05 14:22:13 +0200

    add test component

--- got_sentiment/3_sentiment_analysis/kubeflow/kfp_pipeline.py
+++ got_sentiment/3_sentiment_analysis/kubeflow/kfp_pipeline.py
@@ -1,7 +1,9 @@
 from kfp import dsl
+from kfp import components as comp
+from kfp.components import InputPath, InputTextFile, OutputPath, OutputTextFile
 
 import kfp
-client = kfp.Client(host='https://5fce22797569c67c-dot-europe-west1.pipelines.googleusercontent.com')
+client = kfp.Client(host='https://133bb6ab465f6054-dot-europe-west1.pipelines.googleusercontent.com')
 
 def preprocess_op():
 
@@ -11,18 +13,16 @@ def preprocess_op():
         arguments=[],
         file_outputs={'preprocessed-dir': '/Preprocess/preprocess-data-dir.txt'}
     )
-def train_op(file):
+def train_op(preprocess_data_dir : str):
 
-    preprocess_data_dir =(file.read())
+    preprocess_data_dir
     return dsl.ContainerOp(
         name='Train Model',
         image='abouzid/gcp-project-trainer:latest',
         arguments=['--preprocessed-dir', preprocess_data_dir],
         file_outputs={'model-dir': '/trainer/model-dir.txt'}
     )
-def test_op(file_test_data, file_model):
-    preprocess_data_dir =(file_test_data.read())
-    model_dir =(file_model.read())
+def test_op(preprocess_data_dir : str , model_dir):
     return dsl.ContainerOp(
         name='Test Model',
         image='abouzid/gcp-project-test-model:latest',
@@ -31,7 +31,7 @@ def test_op(file_test_data, file_model):
             '--model-dir', model_dir
         ],
         file_outputs={
-            'performance-file': '/test/performance-model.txt.txt'
+            'performance-file': '/test/performance-model.txt'
         }
     )
 @dsl.pipeline(
@@ -41,11 +41,9 @@ def test_op(file_test_data, file_model):
 def Twitter_Sentimental_Pipeline():
     _preprocess_op = preprocess_op()
     _train_op = train_op(
-        dsl.InputArgumentPath(_preprocess_op.outputs['preprocessed-dir'])
+        _preprocess_op.outputs['preprocessed-dir']
     ).after(_preprocess_op)
-    _test_op = test_op(
-        dsl.InputArgumentPath(_preprocess_op.outputs['preprocessed-dir']),
-        dsl.InputArgumentPath(_train_op.outputs['model-dir'])
+    _test_op = test_op(_preprocess_op.outputs['preprocessed-dir'],_train_op.outputs['model-dir']
     ).after(_train_op)
 
 client.create_run_from_pipeline_func(Twitter_Sentimental_Pipeline, arguments={})

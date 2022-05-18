349b43435d4eed3c8bb23d9a7563d72bed6eb9a3
ABOUZID <bouzidanas@yahoo.fr>
2022-05-18 14:27:19 +0200

    add use case 1

--- Level_1/Test_model/test_model.py
+++ Level_1/Test_model/test_model.py
@@ -3,11 +3,24 @@ import pandas as pd
 import datetime
 import numpy as np
 
+
 def read_test_data(uri):
     y_test = pd.read_csv(uri + '/y_test.csv', encoding="latin1", header=None).to_numpy()
     x_test = pd.read_csv(uri + '/x_test.csv', encoding="latin1", header=None).to_numpy()
 
     return y_test, x_test
+
+
+def valid_model(model, y_test, x_test, hparams):
+    [loss, acc] = model.evaluate(x_test, y_test)
+    scores = model.predict(x_test)
+    predictions = np.array([int(np.round(i)) for i in scores])
+    confu_matrix = tf.math.confusion_matrix(predictions, y_test)
+    std_pred = np.std(scores)
+    if acc > hparams["acc"] and std_pred > hparams["std"]:
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        tf.saved_model.save(model, (hparams['model-validation-dir']) + '_' + nowTime)
+    return loss, acc, confu_matrix, std_pred
 
 
 def evaluate_model(hparams):
@@ -19,18 +32,10 @@ def evaluate_model(hparams):
     """
 
     y_test, x_test = read_test_data(hparams['preprocess-data-dir'])
-    model = tf.keras.models.load_model(hparams['model-dir'],custom_objects={'tf': tf})
+    model = tf.keras.models.load_model(hparams['model-dir'], custom_objects={'tf': tf})
 
-    [loss, acc] = model.evaluate(x_test, y_test )
-    scores = model.predict(x_test)
-    predictions = np.array([int(np.round(i)) for i in scores ])
-    confusion_matrix=tf.math.confusion_matrix(predictions, y_test)
-    std_prediction = np.std(scores)
-
-    if acc > hparams['performance-threshold']:
-        nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        tf.saved_model.save(model, (hparams['model-validation-dir']) + '_' + nowTime)
+    [loss, acc, confusion_matrix, std_prediction] = valid_model(model, y_test, x_test, hparams['performance-threshold'])
 
     ouputfile = open("performance-model.txt", "w")
-    ouputfile.write(str("loss : "+str(loss)+"/n acc :"+str(acc)+"/n matrix-co :"+str(confusion_matrix)
-                        +"/n std-prediction :"+str(std_prediction)) )
+    ouputfile.write(str("loss : " + str(loss) + "/n acc :" + str(acc) + "/n matrix-co :" + str(confusion_matrix)
+                        + "/n std-prediction :" + str(std_prediction)))
--- Level_1/trainer/task.py
+++ Level_1/trainer/task.py
@@ -8,15 +8,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Vertex custom container training args. These are set by Vertex AI during training but can also be overwritten.
     parser.add_argument('--model-dir', dest='model-dir',
-                         type=str, help='Model dir.')
+                        type=str, help='Model dir.')
     parser.add_argument('--bucket', dest='bucket',
                         default="'rare-result-248415-tweet-sentiment-analysis'", type=str, help='bucket name.')
-    parser.add_argument('--preprocess-data-dir', dest='preprocess-data-dir', type=str, help='Training data GCS or BQ URI set during Vertex AI training.')
+    parser.add_argument('--preprocess-data-dir', dest='preprocess-data-dir', type=str,
+                        help='Training data GCS or BQ URI set during Vertex AI training.')
     parser.add_argument('--temp-dir', dest='temp-dir', type=str, help='Temp dir set during Vertex AI training.')
     # Model training args.
-    parser.add_argument('--batch-size', dest='batch-size', default=128, type=int, help='Number of examples during each training iteration.')
-    parser.add_argument('--load_json',dest='load_json',default='confg.json',
-                    help='Load settings from file in json format. Command line options override values in file.')
+    parser.add_argument('--batch-size', dest='batch-size', default=128, type=int,
+                        help='Number of examples during each training iteration.')
+    parser.add_argument('--load_json', dest='load_json', default='confg.json',
+                        help='Load settings from file in json format. Command line options override values in file.')
 
     args = parser.parse_args()
     if args.load_json:
